884a6efa0c72490044e882b3e45fa2ca84f22f42
Lavya <lavya203@gmail.com>
2026-02-04 16:19:09 +0530

    frontend set up

---
+++ Frontend/myapp/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Home from './pages/Home.jsx'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <Home>
+      
+     </Home>
+    </>
+  )
+}
+
+export default App
---
+++ Frontend/myapp/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function page() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     
+    </>
+  )
+}
+
+export default page
